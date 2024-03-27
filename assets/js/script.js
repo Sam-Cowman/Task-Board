6fd4a56af90c5799d819fdfe8f6ba7eaa9b1624e
Sam-Cowman <sam.p.cowman@gmail.com>
2024-03-27 11:27:58 -0500

    Added Javascript and notes for what to add

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,22 +7,44 @@ const addBtnEl = $('#add-btn');
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+return new Date().getTime()
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    // use jquery to create a div and add class card 
+    // what should be on the card, h tag for, create the html elements here 
+    // append smaller elements to the card
 
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+    // append smaller elements to the card 
+    // get tasks from local storage 
+    // empty all the divs with jquery 
+    // empty the cards before adding cards 
 
 }
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+// need to get info for form, create a task object
+// be able to add task to task list 
+// render task list 
+event.preventDefault()
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 
+const newTask = { 
+    name: $('#taskName').val(),
+    date: $('#taskDueDate').val(),
+    status: 'todo',
+    description: $('#taskDescription').val(),
+    id: generateTaskId(),
+}
+taskList.push(newTask)
+localStorage.setItem('tasks', JSON.stringify(taskList))
+console.log (newTask)
 }
 
 // Todo: create a function to handle deleting a task
@@ -37,7 +59,7 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    taskFormEl.on('submit', handleAddTask)
    
 });
 
